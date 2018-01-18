0ba7a873b1584a31da69d295027cfccfb3068ec1
Ryan OKeefe <ryanokeefe208@gmail.com>
2018-01-17 23:13:09 -0500

    inputs

--- src/Validator.js
+++ src/Validator.js
@@ -2,11 +2,33 @@ import React, { Component } from 'react';
 import "./Validator.css";
 
 class Validator extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      email: '',
+      password: '',
+      passwordConfirm: '',
+      valid: true
+    }
+  }
+  
+  handleValidate() {
+    
+  }
+  
+  handleSubmit(e) {
+    e.preventDefault()
+    this.handleValidate()
+  }
+
   render() {
     return (
-      <div className="form">
+      <div className="form" onSubmit={this.handleSubmit}>
         <h1>Sign Up</h1>
-        <input type="password" placeholder="password" />
+        <input className="email" type="email" placeholder="email" value={this.state.value}/>
+        <input className="password" type="password" placeholder="password" value={this.state.value}/>
+        <input className="passwordConfirm" type="passwordConfirm" placeholder="confirm password" value={this.state.value}/>
+        <input type="submit" placeholder="submit" />
       </div>
     );
   }
