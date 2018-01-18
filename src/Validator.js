541b66a3ece9e758ea9b732a98b2cf799b19de35
Ryan OKeefe <ryanokeefe208@gmail.com>
2018-01-18 17:09:38 -0500

    working password validation

--- src/Validator.js
+++ src/Validator.js
@@ -10,11 +10,17 @@ class Validator extends Component {
       passwordConfirm: '',
       valid: true
     }
+    // bind statements: 
+    this.handlePassword = this.handlePassword.bind(this)
+    this.handlePasswordConfirm = this.handlePasswordConfirm.bind(this)
+    this.handleValidate = this.handleValidate.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleEmail = this.handleEmail.bind(this)
   }
-  // bind statements: 
+
 
   // (e)vent needed when setting from input event
-  handleEmail(e) {
+  handleEmail (e) {
     this.setState({
       email: e.target.value
     })
@@ -22,53 +28,54 @@ class Validator extends Component {
   }  
 
   // (e)vent needed when setting from input event
-  handlePassword(e) {
+  handlePassword (e) {    
     this.setState({
-      password: e.target.password
+      password: e.target.value
     })
     console.log(e.target.value)
+    console.log(this.state.password)
     
   }
 
   // (e)vent needed when setting from input event
-  handlePasswordConfirm(e) {
+  handlePasswordConfirm (e) {
     this.setState({
       passwordConfirm: e.target.value
     })
     console.log(e.target.value)
   }
 
-
-  // (e)vent NOT needed: only Evaluating state vs state with ternary op
-  handleValidate() {
-    if (this.state.password === this.state.passwordConfirm) {
-      this.setState({valid: true})
-      console.log("valid password")  
-    } else {
-      this.setState({valid: false})
-      console.log("invalid password")     
-    }
-  }
-
   // on submit: call handleValidate: 
   // (e)vent only needed when firing or setting
-  handleSubmit(e) {
+  handleSubmit (e) {
     e.preventDefault()
     console.log('submit button fired')
     this.handleValidate()
   }
 
+  // (e)vent NOT needed: only Evaluating state vs state with ternary op
+  handleValidate () {
+    this.setState(this.state.password === this.state.passwordConfirm ? {valid: true} : {valid: false})
+
+  }
+
+
   render() {
+    // var to be displayed if passes match or not:
+    let validText = this.state.valid === true ? "Password is valid" : "Passwords do not match"
+
     return (
       // attach handle() methods to each input
 // had to check solution branch :/
       <div className="form">
         <h1>Sign Up</h1>
 
-        <input onChange={(e) => this.handleEmail(e)}className="email" type="email" placeholder="email" />
-        <input onChange={(e) => this.handlePassword}className="password" type="password" placeholder="password" />
-        <input onChange={(e) => this.handlePasswordConfirm}className="passwordConfirm" type="password" placeholder="confirm password" />
-        <input type="submit" onClick={(e) => this.handleSubmit} value="submit" />
+        <input onChange={(e) => this.handleEmail(e)} type="email" placeholder="email" />
+        <input onChange={this.handlePassword} type="password" placeholder="password" />
+        <input onChange={this.handlePasswordConfirm} type="password" placeholder="confirm password" />
+        <input type="submit" onClick={this.handleSubmit} value="submit" />
+
+        <p>{validText}</p>
 
       </div>
     );
@@ -76,3 +83,4 @@ class Validator extends Component {
 }
 
 export default Validator;
+
